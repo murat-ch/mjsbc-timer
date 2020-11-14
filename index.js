bd5b6a0572ee29c2285f435390128c302af159f0
murat-ch <muratik.ch@gmail.com>
2020-11-14 10:33:26 -0500

    1- Timer class

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi there!');
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        console.log('Here');
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        this.startButton.addEventListener('click', this.start.bind(this)); // bind this to the instance of Timer not to strtButton
+        console.log(this);
+
+    }
+
+    start() {
+        console.log('Strt timer!');
+        console.log(this);
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
+timer.start();
